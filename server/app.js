470e408a970a426a12baad64a1e5ec2debe3dbd9
sean-johnson <hello@seanjohnson.nz>
2016-08-11 18:07:49 +1200

    setting up basic express server

---
+++ server/app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const path = require('path')
+const colors = require('colors')
+
+const app = express()
+
+app.use(express.static('./public'))
+
+app.use('/', function (req, res) {
+  res.sendFile(path.resolve('public/index.html'))
+})
+
+const port = process.env.PORT || 3000
+
+console.log('-----------------------'.grey)
+console.log('Server status: Ready'.green)
+console.log('-----------------------'.grey)
+console.log('Listening on port:'.green, port)
+console.log('-----------------------'.grey)
+
+app.listen(port, function(error) {
+  if (error) {
+    console.log("Port error: ".red, error)
+  }
+})
